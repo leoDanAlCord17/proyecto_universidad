ac57a1c061cade81c8c4a59ebd881332fb9aa117
Leo Alvarez <leoalvarez1111s@gmail.com>
2026-06-08 13:54:47 -0400

    fix: token FCM con onConflict correcto y debug de notificaciones push

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyD7NInOIx0MmiWCkxHw1wrAICvm_zf_kT4",
