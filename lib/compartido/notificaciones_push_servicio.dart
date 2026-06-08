3b655a7064b13b9ab5ccd2775ca4145616b382a8
Leo Alvarez <leoalvarez1111s@gmail.com>
2026-06-08 13:57:00 -0400

    style: dart format notificaciones_push_servicio

--- lib/compartido/notificaciones_push_servicio.dart
+++ lib/compartido/notificaciones_push_servicio.dart
@@ -53,8 +53,11 @@ class NotificacionesPushServicio {
         log.i('Notificación recibida en primer plano: ${notif.title}');
       });
     } catch (e, st) {
-      log.e('Error al inicializar notificaciones push',
-          error: e, stackTrace: st,);
+      log.e(
+        'Error al inicializar notificaciones push',
+        error: e,
+        stackTrace: st,
+      );
     }
   }
 }
