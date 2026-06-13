a5d90ec2f64f057c46666467384170fac1c632ef
Leo Alvarez <leoalvarez1111s@gmail.com>
2026-06-13 06:18:58 -0400

    correcion de bug swipe

--- lib/funcionalidades/autenticacion/login_pantalla.dart
+++ lib/funcionalidades/autenticacion/login_pantalla.dart
@@ -74,7 +74,7 @@ class _CuerpoLogin extends StatelessWidget {
             const SizedBox(height: 45),
             CampoTextoApp(
               etiqueta: 'Correo electrónico',
-              hintText: 'leoalvarez20s@gmail.com',
+              hintText: 'leoalvarez21s@gmail.com',
               controller: correoController,
             ),
             const SizedBox(height: 20),
