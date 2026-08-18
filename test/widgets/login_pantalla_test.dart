3805d6b7c7d26d19912f49b9b34b2f287b1815e5
Deiver Pernia <deivermph@gmail.com>
2026-08-18 00:23:25 -0500

    fix: actualizar tests de widgets para reflejar cambios de textos en login y registro

--- test/widgets/login_pantalla_test.dart
+++ test/widgets/login_pantalla_test.dart
@@ -35,7 +35,7 @@ void main() {
 
       await tester.pumpWidget(_marco(loginCubit, authCubit));
 
-      expect(find.text('Cédula'), findsOneWidget);
+      expect(find.text('Cédula de identidad'), findsOneWidget);
       expect(find.text('Contraseña'), findsOneWidget);
       expect(find.text('Iniciar sesión'), findsOneWidget);
     });
